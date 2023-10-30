ea4341cf3bc3d556b031cde67539dc5528bce198
Tuyen Dang <tuyendq@gmail.com>
2023-10-30 22:21:32 +0700

    config.py: get values from .env file

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 auth_type = 'Bearer'
-consumerID = ''
-consumerSecret = ''
+consumerID = os.getenv('CONSUMER_ID')
+consumerSecret = os.getenv('CONSUMER_SECRET')
 
 url = 'https://fc-data.ssi.com.vn/'
 stream_url = 'https://fc-datahub.ssi.com.vn/'
